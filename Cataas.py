0d2205ac7c72d37e2c99a2223081d0323581c495
лиза <kochneva403@GMAIL.COM>
2024-11-25 21:48:01 +0500

    Прописали основы кода без функции вывода картинки load_image

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,20 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO # Чтобы превратить картинку в нормальное изображение
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x400")
+
+Label = Label() # Создаем метку на которой будет выводиться
+Label.pack()
+
+url = "https://cataas.com/cat"
+img = load_image() # функция загрузки изображения в который передаем url
+
+if img:
+    label.config(image=img)
+    label.image = img # Чтобы сборщик мусора не выбросил нашу картинку
+
+window.mainloop()
